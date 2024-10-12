669762d030a2eba9ea134410b481b844a51ee9d7
Murray Brown <gmbchessacc@gmail.com>
2024-10-12 10:12:49 +0100

    Add files via upload

---
+++ Kotlin (Activities)/Very Easy/MatPracticePaperOne.kt
@@ -0,0 +1,28 @@
+package com.example.studified.activities.maths
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.app.ActivityOptionsCompat
+import com.example.studified.R
+import com.example.studified.activities.english.EnglishPage
+
+class MatPracticePaperOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mat_practice_paper_one)
+
+        val backButton: ImageView = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            backNav()
+        }
+    }
+
+    private fun backNav()
+    {
+        val options = ActivityOptionsCompat.makeCustomAnimation(this, 0, 0)
+        val intent = Intent(this, MathsPage::class.java)
+        startActivity(intent, options.toBundle())
+    }
+}
---
+++ Kotlin (Activities)/Very Easy/MathTrigonometry.kt
@@ -0,0 +1,28 @@
+package com.example.studified.activities.maths
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.app.ActivityOptionsCompat
+import com.example.studified.R
+import com.example.studified.activities.english.EnglishPage
+
+class MathTrigonometry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_math_trigonometry)
+
+        val backButton: ImageView = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            backNav()
+        }
+    }
+
+    private fun backNav()
+    {
+        val options = ActivityOptionsCompat.makeCustomAnimation(this, 0, 0)
+        val intent = Intent(this, MathsPage::class.java)
+        startActivity(intent, options.toBundle())
+    }
+}
