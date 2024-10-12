1c5ca23224f3437cc28ebb529fc4ffcd19ab7796
Murray Brown <gmbchessacc@gmail.com>
2024-10-12 10:10:13 +0100

    Add files via upload

---
+++ Kotlin (Activities)/Very Easy/EngTenMarkerQuestions.kt
@@ -0,0 +1,26 @@
+package com.example.studified.activities.english
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.app.ActivityOptionsCompat
+import com.example.studified.R
+
+class EngTenMarkerQuestions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_eng_ten_marker_questions)
+
+        val backButton: ImageView = findViewById(R.id.backButton)
+        backButton.setOnClickListener {
+            backNav()
+        }
+    }
+    private fun backNav()
+    {
+        val options = ActivityOptionsCompat.makeCustomAnimation(this, 0, 0)
+        val intent = Intent(this, EnglishPage::class.java)
+        startActivity(intent, options.toBundle())
+    }
+}
